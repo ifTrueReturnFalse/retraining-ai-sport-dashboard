63baa2fd4b565b0e09e51e336d83fe008463bc35
Timothe Duquesne <timothe.duquesne@gmail.com>
2025-09-20 14:47:53 +0200

    feat: enhance dashboard layout by adding Image component and refining styles

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/app/ui/Buttons/Buttons";
+import Image from "next/image";
 import styles from "./dashboard.module.css";
 
 export default function Page() {
   return (
-    <div>
+    <div className={styles.dashboard}>
       <div className={styles.chatbotHint}>
-        <p>
+        <Image src="/stars.svg" alt="Nice little stars to feel the IA magic !" width={19} height={20.46} className="mr-5" />
+        <p className="mr-15">
           Posez vos questions sur votre programme, vos performances ou vos
           objectifs.
         </p>
